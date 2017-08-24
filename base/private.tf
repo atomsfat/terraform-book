c032c16ab9ddeec29856daeecdc2ef82b613f99e
atomsfat <atomsmail@gmail.com>
2017-08-24 17:42:39 -0500

    Allow instances to see internet

--- base/private.tf
+++ base/private.tf
@@ -20,7 +20,21 @@ resource "aws_security_group" "db"{
     protocol = "icmp"
     cidr_blocks = ["${var.vpc_cidr}"]
   }
-  
+
+  #allow external trafic to the world
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   vpc_id = "${aws_vpc.default.id}"
 
   tags {
--- base/public.tf
+++ base/public.tf
@@ -32,6 +32,19 @@ resource "aws_security_group" "web"{
     protocol = "tcp"
     cidr_blocks = ["${var.private_subnet_cidr}"]
   }
+  #trafic to the world
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   vpc_id = "${aws_vpc.default.id}"
 
